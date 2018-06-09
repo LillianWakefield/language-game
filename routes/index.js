989a2bb2f406d168e204169328fd594b7b9a3a23
James Gottschalk <jgottschalk@riskalyze.com>
2018-06-09 13:14:14 -0400

    oops i crashed the app. Fixed by commenting out some things that aren't there yet

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const setupAuth = require('./auth');
+//const setupAuth = require('./auth');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
