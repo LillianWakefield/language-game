dce275b26d8b2fd5392815b831ef09a9f96248bc
Adrian Wiszniewski <Adrian.Wiszniewski@ricoh-usa.com>
2018-06-11 14:54:49 -0400

    added dummy routes and completed quetion card home page with dummy data

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Chilangos' });
 });
-
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'stuff here for github' });
+});
+router.get('/home', function(req, res, next) {
+  res.render('home', {phrase:'hasta la madre', //show random question card for authenticated user
+                      answer1: "goodbye mommy!",
+                      answer2: "your mother!",
+                      answer3: "to be fed up!",
+                      answer4: "what a mother!",
+                      score: 0 });
+});
 module.exports = router;
