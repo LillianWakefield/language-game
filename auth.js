359e0e01dd3bbb1ba53e6820f557cebcdb18e7f3
Adrian Piotrek Wiszniewski <adrianw.ct@gmail.com>
2018-06-14 11:08:15 -0400

    changed callback url to go to home after authentication

--- auth.js
+++ auth.js
@@ -22,7 +22,7 @@ const setupAuth = (app) => {
     passport.use(new GitHubStrategy({
         clientID: "9c6f9733180638093a3e",
         clientSecret: "dd3676a334855c0b6db74e7550f38627d112c93c",
-        callbackURL: "https://chilangosproj.herokuapp.com/login"
+        callbackURL: "https://chilangosproj.herokuapp.com/home"
     }, (accessToken, refreshToken, profile, done) => {
         User.findOrCreate({
             where: {
