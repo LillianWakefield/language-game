7af8931da2d9848b302e88958fb9400fd792af4e
James <jamesgott@gmail.com>
2018-06-13 20:52:51 -0400

    running out of ideas

--- auth.js
+++ auth.js
@@ -52,7 +52,7 @@ const setupAuth = (app) => {
         res.redirect('/');
     });
 
-    app.get('/github/auth',
+    app.get('/login',
         passport.authenticate('github', { failureRedirect: '/login'}),
         (req, res) => {
             res.redirect('/');
